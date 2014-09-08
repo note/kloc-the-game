4e43bdabd929d1eb72b9c3584d5cfad8c7b929a6
Michał Sitko <pierwszy1@gmail.com>
2014-09-08 22:28:50 +0200

    Rook implemented

--- web/public/javascripts/game-rules/chessboard.js
+++ web/public/javascripts/game-rules/chessboard.js
@@ -40,19 +40,23 @@ define(['underscore'], function(_){
 
     Field.prototype.toIndex = function() {
         return 8 * this.row + this.column;
+    };
+
+    Field.prototype.addVector = function(vector) {
+        return new Field(this.column + vector.x, this.row + vector.y);
     }
 
     Field.prototype.sameRow = function(anotherField) {
         return this.row === anotherField.row;
-    }
+    };
 
     Field.prototype.sameColumn = function(anotherField) {
         return this.column === anotherField.column;
-    }
+    };
 
     Field.prototype.toString = function() {
         return "Field(" + String.fromCharCode(asciiValueOfA + this.column) + (this.row + 1).toString + ")"; // row + 1 because we want to dispay row 0 as 1 and so on
-    }
+    };
 
     var Chessboard = function() {
         this.fields = new Array(64)
@@ -66,9 +70,66 @@ define(['underscore'], function(_){
         return this.fields[field.toIndex()];
     };
 
+    function getFieldsByVector(from, to, vector) {
+        var fields = [];
+        var currentField = from.addVector(vector);
+        while(!_.isEqual(currentField, to)){
+            fields.push(currentField);
+            currentField = currentField.addVector(vector);
+        }
+        return fields;
+    }
+
+    function verticalVector(from, to) {
+        return from.row < to.row ? {x: 0, y: 1} : {x: 0, y: -1};
+    }
+
+    function horizontalVector(from, to) {
+        return from.column < to.column ? {x: 1, y: 0} : {x: -1, y: 0};
+    }
+
+    function sameDiagonal(from, to) {
+        return Math.abs(to.row - from.row) === Math.abs(to.column - from.column);
+    }
+
+    function diagonalVector(from, to) {
+        var vector = {};
+        vector.x = from.column < to.column ? 1 : -1;
+        vector.y = from.row < to.row ? 1 : -1;
+        return vector;
+    }
+
+    function getFieldsBetween(from, to) {
+        if(from.column === to.column){
+            return getFieldsByVector(from, to, verticalVector(from, to));
+        }
+
+        if(from.row === to.row){
+            return getFieldsByVector(from, to, horizontalVector(from, to));
+        }
+
+        if(sameDiagonal(from, to)){
+            return getFieldsByVector(from, to, diagonalVector(from, to));
+        }
+
+        return new Error("getFieldsBetween called with illegal arguments");
+    }
+
+    Chessboard.prototype.somethingBetween = function(from, to) {
+        var found = _.find(getFieldsBetween(from, to), function(field) {
+            return this.getPiece(field) !== undefined;
+        }, this);
+        return found !== undefined;
+    }
+
     Chessboard.prototype.isLegalMove = function(move) {
         var fromField = move.from.toIndex();
+        var toField = move.to.toIndex();
         if(!this.fields[fromField]){
+            return false;
+        }
+
+        if(this.fields[toField] && this.fields[fromField].color === this.fields[toField].color){
             return false;
         }
 
@@ -97,7 +158,7 @@ define(['underscore'], function(_){
     };
 
     Rook.prototype.isLegalMove = function(chessboard, move) {
-        return move.from.sameRow(move.to) || move.from.sameColumn(move.to);
+        return (move.from.sameRow(move.to) || move.from.sameColumn(move.to)) && !chessboard.somethingBetween(move.from, move.to);
     }
 
     var Color = Object.freeze({
--- web/public/javascripts/tests/specs/chessboard.spec.js
+++ web/public/javascripts/tests/specs/chessboard.spec.js
@@ -57,6 +57,51 @@ define(['game', 'underscore', 'jquery'],
             });
         });
 
+        describe("Method Chessboard.somethingBetween", function() {
+            it("returns false if there is no piece between given two fields", function(){
+                var chessboard = new Game.Chessboard();
+                expect(chessboard.somethingBetween(new Game.Field("e1"), new Game.Field("e5"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("f7"), new Game.Field("f1"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("b2"), new Game.Field("h2"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("g3"), new Game.Field("f3"))).toBe(false);
+
+                chessboard.setPiece(new Game.Field("e4"), new Game.Rook(Game.Color.white));
+                expect(chessboard.somethingBetween(new Game.Field("e1"), new Game.Field("e4"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("e4"), new Game.Field("e8"))).toBe(false);
+            });
+
+            it("returns true if there is a piece between given to fields", function(){
+                var chessboard = new Game.Chessboard();
+                chessboard.setPiece(new Game.Field("e4"), new Game.Rook(Game.Color.white));
+                expect(chessboard.somethingBetween(new Game.Field("e1"), new Game.Field("e5"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("e6"), new Game.Field("e3"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("d4"), new Game.Field("f4"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("f4"), new Game.Field("a4"))).toBe(true);
+            });
+
+            it("works for diagonals too", function(){
+                var chessboard = new Game.Chessboard();
+                chessboard.setPiece(new Game.Field("e4"), new Game.Rook(Game.Color.white));
+                expect(chessboard.somethingBetween(new Game.Field("d3"), new Game.Field("f5"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("f5"), new Game.Field("d3"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("d5"), new Game.Field("f3"))).toBe(true);
+                expect(chessboard.somethingBetween(new Game.Field("f3"), new Game.Field("d5"))).toBe(true);
+
+                expect(chessboard.somethingBetween(new Game.Field("a3"), new Game.Field("c5"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("c5"), new Game.Field("a3"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("a5"), new Game.Field("c3"))).toBe(false);
+                expect(chessboard.somethingBetween(new Game.Field("c3"), new Game.Field("a5"))).toBe(false);
+            });
+
+            it("throws an exception when given to fields are not neither on same column nor row nor diagonal", function(){
+                var chessboard = new Game.Chessboard();
+
+                var callSomethingBetween = function() {
+                    chessboard.somethingBetween(new Game.Field("e2"), new Game.Field("d4"));
+                }
+            });
+        })
+
         describe("Rook", function() {
             it("can move vertically", function() {
                 var chessboard = new Game.Chessboard();
@@ -88,6 +133,15 @@ define(['game', 'underscore', 'jquery'],
                 expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("c4")))).toBe(true);
             });
 
+            it("cannot take same color piece", function() {
+                var chessboard = new Game.Chessboard();
+                chessboard.setPiece(new Game.Field("c2"), new Game.Rook(Game.Color.black));
+                chessboard.setPiece(new Game.Field("c4"), new Game.Rook(Game.Color.black));
+
+                // TODO: use matchers
+                expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("c4")))).toBe(false);
+            });
+
             it("cannot overleap", function() {
                 var chessboard = new Game.Chessboard();
                 chessboard.setPiece(new Game.Field("c2"), new Game.Rook(Game.Color.black));
@@ -95,7 +149,6 @@ define(['game', 'underscore', 'jquery'],
                 chessboard.setPiece(new Game.Field("e2"), new Game.Rook(Game.Color.white));
 
                 // TODO: use matchers
-                expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("c4")))).toBe(false);
                 expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("c5")))).toBe(false);
                 expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("c8")))).toBe(false);
                 expect(chessboard.isLegalMove(new Game.Move(new Game.Field("c2"), new Game.Field("f2")))).toBe(false);
