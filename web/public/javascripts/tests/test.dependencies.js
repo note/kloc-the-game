4546cf1fe508f4f16ae584960fa33d5051cc6a93
Michał Sitko <pierwszy1@gmail.com>
2016-09-15 23:46:36 +0200

    just for review

--- web/public/javascripts/tests/test.dependencies.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by michal on 08/09/14.
- */
-
-// Require.js + config
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "require-2.1.15.js");
-EnvJasmine.loadGlobal(EnvJasmine.testDir + "require.conf.js");
